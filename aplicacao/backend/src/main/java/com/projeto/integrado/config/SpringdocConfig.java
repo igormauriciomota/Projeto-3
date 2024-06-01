e806c9443b48ebd77eb59cf04bb5b1d4bdd65586
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-06-01 11:23:18 -0300

    Atualização projeto spring

--- aplicacao/backend/src/main/java/com/projeto/integrado/config/SpringdocConfig.java
+++ aplicacao/backend/src/main/java/com/projeto/integrado/config/SpringdocConfig.java
@@ -1,20 +1,22 @@
-package com.projeto.integrado.config;
+package org.springframework.context.annotation;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.License;
 
-@Configuration
+@Component
+@Configurable
 public class SpringdocConfig {
 	@Bean
 	  public OpenAPI springShopOpenAPI() {
 	      return new OpenAPI()
               .info(new Info().title("API Restful Projeto Integrado")
               .description("Documentação do Projeto Integrado.")
-              .version("1.0.0")
+              .version("2.5.0")
               .license(new License().name("Apache 2.5").url("https://springdoc.org/")))
               .externalDocs(new ExternalDocumentation()
               .description("Link do Repositório da Aplicação")
